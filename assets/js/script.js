eb98c544b045a5358a50052988fa6a5afe309693
naisepaul <>
2023-08-14 06:10:47 +0000

    Adding colors to all divisions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const cardContainer = document.getElementsByClassName('cards');
-const colors = ["red", "blue", "green", "yellow", "aquamarine", "gold", "teal", "pearl"];
+const colors = ["red", "blue", "green", "yellow", "aquamarine", "gold", "teal", "violet"];
 const colorsPickList = [...colors, ...colors]; //make a copy of colors array
 const cardCount = colorsPickList.length;
 console.log(cardCount);
