53207eeda2399e1a9edf9890b004da578993536a
naisepaul <>
2023-08-16 10:54:34 +0000

    made changes to the game board and heading

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,12 @@ const colors = ["red", "blue", "green", "yellow", "teal", "cyan", "grey", "pink"
 const colorsPickList = [...colors, ...colors]; //make a copy of colors array
 const shufColors = colorsPickList.sort(() => 0.5 - Math.random());  // for randomly select colors
 const cardContainer = document.querySelector('.cards');
+
 for (let i = 0; i < colorsPickList.length; i++) {
     let box = document.createElement('div');
     box.className = 'card';
     box.style.backgroundColor = shufColors[i];
     cardContainer.appendChild(box);
 }
+
+
