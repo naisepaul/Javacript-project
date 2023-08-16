9952e578f66687bda77ebc25a59f0c9417c7c868
naisepaul <>
2023-08-16 08:36:07 +0000

    adding color div using javascript and shuffling when click restart button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
-const cardContainer = document.getElementsByClassName('cards');
-const colors = ["red", "blue", "green", "yellow", "aquamarine", "gold", "teal", "violet"];
+
+const colors = ["red", "blue", "green", "yellow", "teal", "cyan", "grey", "pink"];
 const colorsPickList = [...colors, ...colors]; //make a copy of colors array
-const cardCount = colorsPickList.length;
-console.log(cardCount);
+const shufColors = colorsPickList.sort(() => 0.5 - Math.random());  // for randomly select colors
+const cardContainer = document.querySelector('.cards');
+for (let i = 0; i < colorsPickList.length; i++) {
+    let box = document.createElement('div');
+    box.className = 'card';
+    box.style.backgroundColor = shufColors[i];
+    cardContainer.appendChild(box);
+}
