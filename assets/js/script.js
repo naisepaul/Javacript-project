10395cce1f0e0749f4f2b62ec98b6ddf0d83cc22
naisepaul <>
2023-08-17 22:57:00 +0000

    checking matching cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,52 @@
 
-// shuffling Colors
-const colors = ["red", "blue", "green", "yellow", "teal", "cyan", "grey", "pink"];
-const colorsPickList = [...colors, ...colors]; //make a copy of colors array
-const shufColors = colorsPickList.sort(() => 0.5 - Math.random());  // for randomly select colors
-const cardContainer = document.querySelector('.cards');
-let flippedCard = [];
-for (let i = 0; i < colorsPickList.length; i++) {
-    let box = document.createElement('div');
-    box.className = 'card';
-    box.style.backgroundColor = shufColors[i];
-    cardContainer.appendChild(box);
-    box.addEventListener('click', flipcard);
-    function flipcard() {
-        this.classList.toggle('flip');
-        // flippedCard.append(this.backgroundColor);
-        console.log(this.style.backgroundColor);
-    }
 
-}
+document.addEventListener('DOMContentLoaded', function () {
+
+    // shuffling Colors
+
+    const colors = ["red", "blue", "green", "yellow", "teal", "cyan", "grey", "pink"];
+    const colorsPickList = [...colors, ...colors]; //make a copy of colors array
+    const shufColors = colorsPickList.sort(() => 0.5 - Math.random());  // for randomly select colors
+    const cardContainer = document.querySelector('.cards');
+
+    // making 4*4 grid
+
+    for (let i = 0; i < colorsPickList.length; i++) {
+        let box = document.createElement('div');
+        box.className = 'card';
+        box.style.backgroundColor = shufColors[i];
+        cardContainer.appendChild(box);
+
+        // flipping cards
+
+        let cardColor = box.style.backgroundColor;
+        box.addEventListener('click', () => {
+            box.classList.toggle('flip');
+
+            // checking matching colors
+
+            let flipped = 0;
+            let firstflipped = '';
+            let secondflipped = '';
+            let matchedCards = 0;
+
+            if (flipped === 0) {
+                firstflipped = cardColor;
+                flipped++;
+            } else {
+                secondflipped = cardColor;
+                flipped = 0;
+                if (firstflipped === secondflipped) {
+                    matchedCards++;
+                    const correctCards = ' ';
+                    
+                }
+
+            }
+
+
+
+        });
+
+    }
+});
