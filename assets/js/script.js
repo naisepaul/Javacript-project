0e9d16ce7b31447655abbb89064b58f0661cb6d0
naisepaul <>
2023-08-21 21:41:13 +0000

    create gameboard function and flipcard functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,48 @@
-// document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
 
-const colors = ["red", "blue", "green", "yellow", "teal", "cyan", "grey", "pink"];
-const colorsPickList = [...colors, ...colors]; //double the array
-let flippedCards = [];
-let matchedCards = [];
-let lockboard = false;
-// shuffling Colors
-
-// function shuffle() {
-const shufColors = colorsPickList.sort(() => 0.5 - Math.random());  // for randomly select colors
-const cardContainer = document.querySelector('.cards');
-for (let i = 0; i < colorsPickList.length; i++) {
-    let box = document.createElement('div');
-    box.className = 'card';
-    box.style.backgroundColor = shufColors[i];
-    box.setAttribute('data-color', shufColors[i]);
-    cardContainer.appendChild(box);
-    // flipping cards
-
-    let counter = 0;
-
-    let firstFlipped = '';
-    let secondFlipped = '';
-    let matchedCards = 0;
-    let card1, card2 = [];
-
-    box.addEventListener('click', () => {
-        box.classList.toggle('flip');
-        if (flippedCards.length < 2) {
-            flippedCards.push(box.style.backgroundColor);
-            // console.log(flippedCards);
-            // console.log(flippedCards[1]);
-            if ((flippedCards.length === 2) && (flippedCards[0] === flippedCards[1])) {
-
-                console.log(flippedCards);
-
-            } else {
-
-                console.log(flippedCards[0]);
-                console.log(flippedCards[1]);
-            }
+    const colors = ["red", "blue", "green", "yellow", "teal", "cyan", "grey", "pink"];
+    const colorsPickList = [...colors, ...colors]; //double the array
+    let flippedCards = [];
+    // shuffling Colors
+    const shufColors = colorsPickList.sort(() => 0.5 - Math.random());  // for randomly select colors
+    const cardContainer = document.querySelector('.cards');
+    //creating gameboard
+    function gameBoard() {
+        for (let i = 0; i < colorsPickList.length; i++) {
+            let box = document.createElement('div');
+            box.className = 'card';
+            // box.style.backgroundColor = shufColors[i];
+            box.setAttribute('data-color', shufColors[i]);
+            cardContainer.appendChild(box);
+            box.addEventListener('click', flipCard);
+            box.classList.toggle('flip');
         }
+    }
+
+    function flipCard() {
+        let cardColor = this.getAttribute('data-color');
+        console.log(cardColor);
+
+    }
+    gameBoard();
+    // box.addEventListener('click', () => {
+    //     box.classList.toggle('flip');
+    //     if (flippedCards.length < 2) {
+    //         flippedCards.push(box.style.backgroundColor);
+    //         // console.log(flippedCards);
+    //         // console.log(flippedCards[1]);
+    //         if ((flippedCards.length === 2) && (flippedCards[0] === flippedCards[1])) {
+
+    //             console.log(flippedCards);
+
+    //         } else {
+
+    //             console.log(flippedCards[0]);
+    //             console.log(flippedCards[1]);
+    //         }
+    //     }
 
 
-    });
+});
 
-}
+
